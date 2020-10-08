08c541a88953e08163ec4c253235ec33bc930c18
Haoran Luo <1910567646@qq.com>
2020-10-08 22:13:19 +0800

    feat(client/windows): build windows using mingw32 cross-building method.

--- cmd/client/build.sh
+++ cmd/client/build.sh
@@ -10,7 +10,14 @@ if [ "$GOARCH" == "" ]; then export GOARCH=amd64; fi
 # Build the LuaJIT (checked-out in the workflow).
 echo "Build LuaJIT for link dependencies"
 cd luajit
+if [ "$HOST_CC" == "" ]; then
 $MAKE BUILDMODE=static CC="$CC" clean all install
+else
+$MAKE BUILDMODE=static HOST_CC="$HOST_CC" \
+      XCFLAGS+="$XCFLAGS" CROSS="$CROSS" \
+      TARGET_SYS="$TARGET_SYS" FILE_T="$FILE_T" \
+      clean all install
+fi
 cd -
 
 # Build the client connector archive.
